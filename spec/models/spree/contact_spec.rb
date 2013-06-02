6c23a41bc53a261739943569d6ec0d38575e0c44
Daniel Shimoyama <shimoyama.daniel@gmail.com>
2013-06-02 16:55:09 -0300

    adding some model validations

--- spec/models/spree/contact_spec.rb
+++ spec/models/spree/contact_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Spree::Contact do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should have a valid email" do
+    Spree::Contact.create(email: "test").should_not be_valid
+    Spree::Contact.create(email: "").should_not be_valid
+    Spree::Contact.create(email: "test@test.com.br").should be_valid
+  end
+  
+  it "should have a valid first name" do
+    Spree::Contact.create(first_name: "").should_not be_valid
+    Spree::Contact.create(first_name: "John").should_be_valid
+  end
+  
+  it "should have a valid last name" do
+    Spree::Contact.create(last_name: "").should_not be_valid
+    Spree::Contact.create(last_name: "Anderson").should_be_valid
+  end
+  
+  it "should hava a valid phone" do
+    Spree::Contact.create(phone: "").should_not be_valid
+  end
+
 end
